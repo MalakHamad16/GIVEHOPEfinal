8f81417f9ffd6c44bf4af2da15c1e53ae11d6a4c
MalakHamad16 <166342740+MalakHamad16@users.noreply.github.com>
2025-12-22 15:46:32 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+//Frontend static
+app.use(express.static(path.join(__dirname, '..')));
+
 // ✅ Static files — آمنة ومحددة
 app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
 app.use('/public/sponsor', express.static(path.join(__dirname, '../public/sponsor')));
